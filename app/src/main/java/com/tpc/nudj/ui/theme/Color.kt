15a5af13a27abbaf6812409fad955d634109ffee
Nirvan Jain <nirvanj73@gmail.com>
2026-06-16 13:49:43 +0530

    added color scheme and updated theme.kt

--- app/src/main/java/com/tpc/nudj/ui/theme/Color.kt
+++ app/src/main/java/com/tpc/nudj/ui/theme/Color.kt
@@ -9,3 +9,12 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+
+// App Main Colors
+val LightBlue = Color(0xFFE0E9EF)
+val DarkBlue = Color(0xFF093958)
+val Yellow = Color(0xFFFFCE2D)
+val Gray = Color(0xFFA0B5C2)
+val SeaBlue = Color(0xFF5A8EEF)
--- app/src/main/java/com/tpc/nudj/ui/theme/Theme.kt
+++ app/src/main/java/com/tpc/nudj/ui/theme/Theme.kt
@@ -5,48 +5,62 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
-    background = Color.DarkGray
+
+val LocalAppColors = staticCompositionLocalOf { LightColorScheme }
+
+data class AppColors(
+    // Variables for Pages
+    val background: Color,
+    val primaryButtonColor: Color,
+    val secondaryButtonColor: Color,
+    val tertiaryButtonColor: Color,
+    val primaryButtonTextColor: Color,
+    val secondaryButtonTextColor: Color,
+    val surfaceColor: Color,
+    val buttonBorderColor: Color,
+    val textFieldColor: Color,
+    val textFieldBorderColor: Color,
+)
+private val DarkColorScheme = AppColors(
+    background = LightBlue,
+    primaryButtonColor = DarkBlue,
+    secondaryButtonColor = Color.White,
+    tertiaryButtonColor = SeaBlue,
+    primaryButtonTextColor = Color.White,
+    secondaryButtonTextColor = DarkBlue,
+    surfaceColor = Gray,
+    buttonBorderColor = Gray,
+    textFieldColor = Color.White,
+    textFieldBorderColor = Gray
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    background = Color.White
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val LightColorScheme = AppColors(
+    background = LightBlue,
+    primaryButtonColor = DarkBlue,
+    secondaryButtonColor = Color.White,
+    tertiaryButtonColor = SeaBlue,
+    primaryButtonTextColor = Color.White,
+    secondaryButtonTextColor = DarkBlue,
+    surfaceColor = Gray,
+    buttonBorderColor = Gray,
+    textFieldColor = Color.White,
+    textFieldBorderColor = Gray
 )
 
 @Composable
 fun NudjTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+    CompositionLocalProvider(LocalAppColors provides colorScheme) {
+        MaterialTheme(
+            colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme(),
+            content = content
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
